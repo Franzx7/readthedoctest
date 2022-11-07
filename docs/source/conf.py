058d30e180648a29752c36cc30c98710b25f34ca
Franz AKE <72395650+Franzx7@users.noreply.github.com>
2022-11-07 17:37:08 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'SCALPEL'
+copyright = '2022, Franz AKE'
+author = 'Franz AKE'
 
 release = '0.1'
 version = '0.1.0'
